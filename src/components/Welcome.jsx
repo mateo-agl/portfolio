07c3d8db93d726d8c4f808ce951a3e2df7d81862
mateo-agl <aguilarmateo.1604@gmail.com>
2022-01-12 13:54:50 -0300

    update

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import avatar from "../assets/avatar/avatar.svg";
 
 export const Welcome = () => (
-  <section id="home" className="row py-6 align-items-center">
-    <div className="col-xl-4 d-flex justify-content-center">
+  <section id="home" className="row py-6 justify-content-center align-items-center">
+    <div className="col-xl-4 col-sm-6">
       <img className="w-100 avatar" src={avatar} alt="my avatar"/>
     </div>
-    <div className="col-auto"/>
-    <div className="col">
+    <div className="col-xl-8 col-sm-12">
       <h1 className="mb-5 fs-0">Welcome to my page, I'm Mateo</h1>
       <h4>A Front End Web Developer living in Argentina, passionate about creating interactive websites and looking for new challenges!</h4>
       <div className="float-end mt-5">
